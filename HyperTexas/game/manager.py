727f61f6adf9f098bbf7db1e471358a10be7375c
lirc <419384472@qq.com>
2025-04-14 02:03:09 +0800

    patch

--- HyperTexas/game/manager.py
+++ HyperTexas/game/manager.py
@@ -9,182 +9,28 @@ class Manager:
     def __init__(self):
         self.character_dict = dict()
         self.public_cards = []
+        self.last_used_cards =[]
         self.deck = Deck()
         self.deck.shuffle()
-        self.table_effects = []
+        # self.table_effects = []
         self.active_players = []  # 当前回合活跃的玩家列表
         self.base_chip = 30 * 10000     # 初始30万筹码
-        self.winner_chip_pct = 0.5      # 胜利者的筹码需要达到总筹码的50%
+        self.level = 1      # 每个回合 输的人要额外支付level * 1k的底注
 
     def set_init_chips(self, chip):
         # 设置初始筹码数x万
         self.base_chip = chip * 10000
 
-    def set_winner_chip_pct(self, pct):
-        if pct < 1 and pct > 0:
-            self.winner_chip_pct = pct
+    def GameFinished(self):
+        for character in self.character_dict.values():
+            if character.gold <= 0:
+                return True
+        return False
 
     def FindPlayType(self, cards: list):
         # 根据self.poker_play找到类型
         _type = Score_Name_No_Pair
         return _type
-
-    def GetScorablePokers(self, cards: list) -> list:
-        # 获取可计算得分的扑克
-        scorable_cards = []
-        for card in cards:
-            if self.table_effect == Disable_Heart and card.Color == Poker_Color_Heart:
-                continue
-            if card.table_effect == Disable_Diamond and card.Color == Poker_Color_Diamond:
-                continue
-            if card.table_effect == Disable_Club and card.Color == Poker_Color_Club:
-                continue
-            if card.table_effect == Disable_Spade and card.Color == Poker_Color_Spade:
-                continue
-            if card.table_effect == Disable_Head:
-                if card.Number == Poker_Number_J:
-                    continue
-                if card.Number == Poker_Number_Q:
-                    continue
-                if card.Number == Poker_Number_K:
-                    continue
-                scorable_cards.append(card) 
-        return scorable_cards
-
-    def ScoreCard(self, character: Character, poker: Poker):
-        if poker.Material == Poker_Material_Universal:
-            pass
-        if poker.Material == Poker_Material_Gold:
-            character.gold += 3
-        if poker.Material == Poker_Material_Glass:
-            pass
-        if poker.Material == Poker_Material_Iron:
-            pass
-        if poker.Material == Poker_Material_Stone:
-            character.chip += 50
-        if poker.Material == Poker_Material_Lucky:
-            r = random.random(0, 100)
-            if r < 20:
-                character.mag += 20
-            r = random.random(0, 100)
-            if r < 7:
-                character.gold += 20
-        if poker.Material == Poker_Material_Chip:
-            character.chip += 30
-        if poker.Material == Poker_Material_Magnification:
-            character.mag += 4
-        if poker.Number == Poker_Number_A:
-            character.chip += 11
-        if poker.Number == Poker_Number_2:
-            character.chip += 2
-        if poker.Number == Poker_Number_3:
-            character.chip += 3
-        if poker.Number == Poker_Number_4:
-            character.chip += 4
-        if poker.Number == Poker_Number_5:
-            character.chip += 5
-        if poker.Number == Poker_Number_6:
-            character.chip += 6
-        if poker.Number == Poker_Number_7:
-            character.chip += 7
-        if poker.Number == Poker_Number_8:
-            character.chip += 8
-        if poker.Number == Poker_Number_9:
-            character.chip += 9
-        if poker.Number == Poker_Number_10:
-            character.chip += 10
-        if poker.Number == Poker_Number_J:
-            character.chip += 10
-        if poker.Number == Poker_Number_Q:
-            character.chip += 10
-        if poker.Number == Poker_Number_K:
-            character.chip += 10
-
-    def TrigTableEffect(self, character: Character):
-        if self.table_effect == Enhance_No_Pair:
-            if character.played_type == Score_Name_No_Pair:
-                character.mag *= 2
-        if self.table_effect == Enhance_One_Pair:
-            if character.played_type == Score_Name_One_Pair:
-                character.mag *= 2
-        if self.table_effect == Enhance_Two_Pair:
-            if character.played_type == Score_Name_Two_Pair:
-                character.mag *= 2
-        if self.table_effect == Enhance_Three:
-            if character.played_type == Score_Name_Three:
-                character.mag *= 2
-        if self.table_effect == Enhance_Straight:
-            if character.played_type == Score_Name_Straight:
-                character.mag *= 2
-        if self.table_effect == Enhance_Flush:
-            if character.played_type == Score_Name_Flush:
-                character.mag *= 2
-
-        if self.table_effect == Lower_Level:
-            pass
-        if self.table_effect == Disable_Flush:
-            pass
-        if self.table_effect == Disable_Heart:
-            pass
-        if self.table_effect == Disable_Diamond:
-            pass
-        if self.table_effect == Disable_Club:
-            pass
-        if self.table_effect == Disable_Plum:
-            pass
-        if self.table_effect == Less_Hand:
-            pass
-        if self.table_effect == Disable_Head:
-            pass
-        if self.table_effect == Debuff_Tax:
-            character.gold -= len(character.pokers)
-            if character.gold < 0:
-                character.gold = 0
-        if self.table_effect == Debuff_Half:
-            character.chip = character.chip // 2
-            character.mag = character.mag // 2
-        if self.table_effect == Buff_Small_Bet:
-            pass
-        if self.table_effect == Debuff_Big_Bet:
-            pass
-
-    def Score(self, character: Character) -> int:
-        # 计算得分
-        character.chip = 0
-        character.mag = 0
-        _type = self.FindPlayType()
-        character.played_type = _type
-        scorable = self.GetScorablePokers()
-        character.chip, character.mag = BASE_SCORE[_type]
-        bouns_chip, bouns_mag = LEVEL_BOUNS_SCORE[_type]
-        level = character.level.get(_type) or 0
-        
-        character.chip += bouns_chip * level
-        character.mag += bouns_mag * level
-        for poker in self.poker_play: # type: Poker
-            if poker.id not in scorable:
-                continue
-            self.ScoreCard(character, poker)
-            for joker in character.joker:    # type: Joker
-                joker.Score(character, poker)
-            if poker.Wax == Poker_Wax_Gold:
-                character.gold += 3
-            if poker.Wax == Poker_Wax_Red:
-                self.ScoreCard(character, poker)
-                for joker in character.joker:    # type: Joker
-                    joker.Score(character, poker)
-            if poker.Wax == Poker_Wax_Blue:
-                if len(character.consume) < character.max_consume:
-                    _planet = Planet(_type)
-                    character.consume.append(_planet)
-            if poker.Wax == Poker_Wax_Purple:
-                if len(character.consume) < character.max_consume:
-                    _tarot = Tarot(_type)
-                    character.consume.append(_tarot)
-        for joker in character.joker:    # type: Joker
-            joker.Trig(character)
-        self.TrigTableEffect(character)
-        return character.chip * character.mag
 
     def deal_initial_cards(self):
         """发放初始牌：3张公共牌和每个玩家2张手牌"""
@@ -248,3 +94,139 @@ class Manager:
             # 合并公共牌和手牌进行最终计算
             all_cards = player.pokers + self.public_cards
             self.Score(player)
+
+    
+    def Score(self, character: Character) -> int:
+        # 计算得分
+        _score_info = {'score_type': None, 'score_level': 0, 
+                       'add_chip': 0, 'add_mag': 0,
+                       'mut_mag': 1.0
+                       }
+        _type = self.FindPlayType()
+        _score_info['score_type'] = _type
+        character.played_type = _type
+        scorable = self.GetScorablePokers()
+        character.chip, character.mag = BASE_SCORE[_type]
+        bouns_chip, bouns_mag = LEVEL_BOUNS_SCORE[_type]
+        level = character.level.get(_type) or 0
+        _score_info['score_level'] = level
+        self._before_score(character, _score_info)
+        for poker in self.poker_play: # type: Poker
+            self._score_card(character, poker, _score_info)
+            if poker.Wax == Poker_Wax_Gold:
+                character.gold += 3
+            if poker.Wax == Poker_Wax_Red:
+                self._score_card(character, poker, _score_info)                
+            if poker.Wax == Poker_Wax_Blue:
+                if len(character.consume) < character.max_consume:
+                    _planet = Planet(_type)
+                    character.consume.append(_planet)
+            if poker.Wax == Poker_Wax_Purple:
+                if len(character.consume) < character.max_consume:
+                    _tarot = Tarot(_type)
+                    character.consume.append(_tarot)
+        self._after_score(character, _score_info)
+        return character.chip * character.mag
+
+    def _before_score(self, character: Character, _score_info: dict):
+        pass
+
+    def _after_score(self, character: Character, _score_info: dict):
+        pass
+    
+    def _score_card(self, character: Character, poker: Poker, _score_info: dict):
+        _chip = 0
+        _mag = 0
+        if poker.Material == Poker_Material_Universal:
+            pass
+        if poker.Material == Poker_Material_Gold:
+            character.gold += 3
+        if poker.Material == Poker_Material_Glass:
+            pass
+        if poker.Material == Poker_Material_Iron:
+            pass
+        if poker.Material == Poker_Material_Stone:
+            character.chip += 50
+        if poker.Material == Poker_Material_Lucky:
+            r = random.random(0, 100)
+            if r < 20:
+                character.mag += 20
+            r = random.random(0, 100)
+            if r < 7:
+                character.gold += 20
+        if poker.Material == Poker_Material_Chip:
+            character.chip += 30
+        if poker.Material == Poker_Material_Magnification:
+            character.mag += 4
+        if poker.Number == Poker_Number_A:
+            character.chip += 11
+        if poker.Number == Poker_Number_2:
+            character.chip += 2
+        if poker.Number == Poker_Number_3:
+            character.chip += 3
+        if poker.Number == Poker_Number_4:
+            character.chip += 4
+        if poker.Number == Poker_Number_5:
+            character.chip += 5
+        if poker.Number == Poker_Number_6:
+            character.chip += 6
+        if poker.Number == Poker_Number_7:
+            character.chip += 7
+        if poker.Number == Poker_Number_8:
+            character.chip += 8
+        if poker.Number == Poker_Number_9:
+            character.chip += 9
+        if poker.Number == Poker_Number_10:
+            character.chip += 10
+        if poker.Number == Poker_Number_J:
+            character.chip += 10
+        if poker.Number == Poker_Number_Q:
+            character.chip += 10
+        if poker.Number == Poker_Number_K:
+            character.chip += 10
+        if self.table_effect == Enhance_No_Pair:
+            if character.played_type == Score_Name_No_Pair:
+                character.mag *= 2
+        if self.table_effect == Enhance_One_Pair:
+            if character.played_type == Score_Name_One_Pair:
+                character.mag *= 2
+        if self.table_effect == Enhance_Two_Pair:
+            if character.played_type == Score_Name_Two_Pair:
+                character.mag *= 2
+        if self.table_effect == Enhance_Three:
+            if character.played_type == Score_Name_Three:
+                character.mag *= 2
+        if self.table_effect == Enhance_Straight:
+            if character.played_type == Score_Name_Straight:
+                character.mag *= 2
+        if self.table_effect == Enhance_Flush:
+            if character.played_type == Score_Name_Flush:
+                character.mag *= 2
+
+        if self.table_effect == Lower_Level:
+            pass
+        if self.table_effect == Disable_Flush:
+            pass
+        if self.table_effect == Disable_Heart:
+            pass
+        if self.table_effect == Disable_Diamond:
+            pass
+        if self.table_effect == Disable_Club:
+            pass
+        if self.table_effect == Disable_Plum:
+            pass
+        if self.table_effect == Less_Hand:
+            pass
+        if self.table_effect == Disable_Head:
+            pass
+        if self.table_effect == Debuff_Tax:
+            character.gold -= len(character.pokers)
+            if character.gold < 0:
+                character.gold = 0
+        if self.table_effect == Debuff_Half:
+            character.chip = character.chip // 2
+            character.mag = character.mag // 2
+        if self.table_effect == Buff_Small_Bet:
+            pass
+        if self.table_effect == Debuff_Big_Bet:
+            pass
